fffcac17d992c518dc85cb87ebeb31864d461470
Leah Budzicka <leah.budzicka@soundcloud.com>
2025-07-25 16:01:41 +0200

    chore: add main script

---
+++ mac_lookup.zsh
@@ -0,0 +1,51 @@
+#!/usr/bin/env -S zsh -euf
+# This is free and unencumbered software released into the public domain.
+# For more information, please refer to <https://unlicense.org>
+# SPDX-License-Identifier: Unlicense
+
+# Set sane defaults
+emulate -L zsh
+set -eu
+
+: ${APPLE_SUPPORT_URL:="https://support-sp.apple.com/sp/product?cc="}
+readonly APPLE_SUPPORT_URL
+
+function get_xml_tag tag
+# Return the contents of an XML tag
+# :param tag: the tag to search for
+#
+# :returns: contents of $tag if found, else empty string
+{
+	local tag="${1:?}"
+	
+	# Get input from STDIN
+	while	read -r XML
+	do
+		if	[[ "${XML:?}" =~ "<$tag>(.*)</$tag>" ]]
+		then	print -- "${match[1]}"
+		else	
+			print # newline
+			print -P -- "%F{red}error: tag %B<$tag>%b not found%f" >&2
+		fi
+	done
+}
+
+function main
+# Read serial numbers from STDIN, spit out model names
+{
+	while	read -r SERIAL
+	do
+		local LAST4DIGITS="${SERIAL:$#SERIAL - 4:4}"
+
+		curl -sSL \
+		     -w '\n' \
+		     -X GET "${APPLE_SUPPORT_URL}${LAST4DIGITS}"
+
+		unset LAST4DIGITS
+	done | get_xml_tag configCode
+}
+
+# Run main if not interactive
+if	! [[ -o interactive ]] 
+then	main
+fi
